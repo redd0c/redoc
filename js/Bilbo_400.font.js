95a3030a267a6924fb7b2063b5afa53361ab2614
Paul <45143003+redd0c@users.noreply.github.com>
2018-11-18 15:11:57 +0200

    Add files via upload

---
+++ js/Bilbo_400.font.js
@@ -0,0 +1,23 @@
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright (c) 2011 TypeSETit, LLC (typesetit@att.net), with Reserved Font Name
+ * "Bilbo"
+ * 
+ * Trademark:
+ * Bilbo is a trademark of Robert E. Leuschke.
+ * 
+ * Full name:
+ * Bilbo-Regular
+ * 
+ * Manufacturer:
+ * TypeSETit, LLC
+ * 
+ * Designer:
+ * Robert E. Leuschke
+ * 
+ * License information:
+ * http://scripts.sil.org/OFL
+ */
+Cufon.registerFont({"w":72,"face":{"font-family":"Bilbo","font-weight":400,}});
